fdbf253deb3186533fbc805c25e6257631e8af0a
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2017-03-30 15:37:00 +0500

    wip: pallet rack form

--- imports/ui/components/NewPalletRack.jsx
+++ imports/ui/components/NewPalletRack.jsx
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { browserHistory } from 'react-router';
-import FrameTable from './FrameTable'
-import BaysTable from './BaysTable'
+// import FrameTable from './FrameTable'
+// import BaysTable from './BaysTable'
 
 var ReactBootstrap = require('react-bootstrap');
 var Button = ReactBootstrap.Button;
@@ -11,6 +11,9 @@ var FormGroup = ReactBootstrap.FormGroup;
 var ControlLabel = ReactBootstrap.ControlLabel;
 var FormControl = ReactBootstrap.FormControl;
 var Radio = ReactBootstrap.Radio;
+var Table = ReactBootstrap.Table;
+var FieldGroup = ReactBootstrap.FieldGroup;
+var Input = ReactBootstrap.Input;
 
 export default class NewPalletRack extends Component {
   
@@ -62,27 +65,106 @@ export default class NewPalletRack extends Component {
     return (
           <div>   
           New Pallet Rack
-          <FormGroup className="testbg-1">
-          
-        
+          <form className="testbg-1">
+
           <div className="container-fluid row">
             <div className="col-sm-6 testbg-1">
 
-              <br/>
+              <h4>Settings</h4>
               <div className="well">
-                <div className="container-fluid frame-wrapper center-text testbg-1">  
-                  <h4>Frame</h4>
-                  <FrameTable />
-                </div>
 
-                <div className="container-fluid frame-wrapper center-text testbg-1">  
-                  <h4>Bays</h4>
-                  <BaysTable />
+              <ControlLabel>Rack Details</ControlLabel>
+              <Input
+                type="text"
+                defaultValue="My Name"
+                label="Name"
+                placeholder="Full name"
+                />
+              <FormControl 
+                type="textarea" 
+                placeholder="add details" 
+                controlId="id-rack-details"/>
+              <br />
+                <div className="row">
+                  <div className="col-xs-6">
+                    <ControlLabel>Cost</ControlLabel>
+                    <FormControl 
+                      componentClass="text" 
+                      defaultValue="140"
+                      controlId="id-cost"/>
+                  </div>
+                  <div className="col-xs-6">
+                    <ControlLabel>Project Rate</ControlLabel>
+                    <FormControl 
+                      componentClass="text" 
+                      defaultValue="180"
+                      controlId="id-project-rate"/>
+                  </div>
                 </div>
               </div>
+
+              <h4>Frame</h4>
+              <div className="well">
+                <Table className="borderless table">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Height (ft)</th>
+                        <th>Qty.</th>
+                      </tr>
+
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td><FormControl type="text" placeholder="15" /></td>
+                        <td><FormControl type="text" placeholder="10" /></td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                  </div>
             </div>
             <div className="col-sm-6 testbg-2">
-              <br/>
+                                <h4>Bays</h4>
+                  <div className="well">
+                  <Table className="borderless table">
+                    <thead>
+                      <tr>
+                        <th>#</th>
+                        <th>Length (ft)</th>
+                        <th>Qty.</th>
+                        <th>Levels</th>
+                        <th>Load per Level (kgs)</th>
+                      </tr>
+
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>1</td>
+                        <td><FormControl type="text" placeholder="15" /></td>
+                        <td><FormControl type="text" placeholder="10" /></td>
+                        <td><FormControl type="text" placeholder="5" /></td>
+                        <td><FormControl type="text" placeholder="3000" /></td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td><FormControl type="text" placeholder="15" /></td>
+                        <td><FormControl type="text" placeholder="10" /></td>
+                        <td><FormControl type="text" placeholder="4" /></td>
+                        <td><FormControl type="text" placeholder="3000" /></td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td><FormControl type="text" placeholder="15" /></td>
+                        <td><FormControl type="text" placeholder="10" /></td>
+                        <td><FormControl type="text" placeholder="3" /></td>
+                        <td><FormControl type="text" placeholder="3000" /></td>
+                      </tr>
+                    </tbody>
+                  </Table>
+                  
+              </div>
+
               <div className="well">
                 
                 <FormControl type="text" placeholder="Search" />
@@ -105,7 +187,7 @@ export default class NewPalletRack extends Component {
             </div>
           </div>
           <Button type="submit" onClick={this.handleSubmit2}>Submit</Button>
-          </FormGroup>
+          </form>
           </div>
     );
   }
