579aa8dc3ea6a8e3a1214285b6cb2ab4d72df586
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-04-06 02:30:14 -0700

    wip :ui mocha test

--- imports/ui/components/NewPalletRack.tests.js
+++ imports/ui/components/NewPalletRack.tests.js
@@ -2,13 +2,13 @@
 /* eslint-env mocha */
 
 import { Meteor } from 'meteor/meteor';
-import { Factory } from 'meteor/factory';
+import { Factory } from 'meteor/dburles:factory';
 import React from 'react';
 import { mount } from 'enzyme';
 import { chai } from 'meteor/practicalmeteor:chai';
 import { sinon } from 'meteor/practicalmeteor:sinon';
 import { Random } from 'meteor/random';
-import ListHeader from './ListHeader.jsx';
+// import ListHeader from './ListHeader.jsx';
 
 import { insert } from '../../api/quotes/quotes.js' 
 
@@ -18,12 +18,16 @@ if (Meteor.isClient) {
     let header = null;
     let router = null;
     
-    beforeEach(() => {
-      list = Factory.create('list', { userId: Random.id(), name: 'testing' });
-      router = { push: sinon.stub() };
-      header = mount(<ListHeader list={list} />, {
-        context: { router },
-      });
+    // beforeEach(() => {
+    //   list = Factory.create('quote', { userId: Random.id(), name: 'testing' });
+    //   router = { push: sinon.stub() };
+    //   header = mount(<ListHeader list={list} />, {
+    //     context: { router },
+    //   });
+    // });
+
+    it('should create a new todo when user submits', () => {
+      console.log("is this working ?")
     });
 
   });
