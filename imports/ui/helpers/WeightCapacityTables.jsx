16db0ad4e1612566c0555ed22ac784ed7fb179c0
Muhammad Abbas <mohammed.petiwala52@gmail.com>
2017-04-22 01:18:09 -0700

    wip: copy beamWeightCapacityTable

--- imports/ui/helpers/WeightCapacityTables.jsx
+++ imports/ui/helpers/WeightCapacityTables.jsx
@@ -10,9 +10,19 @@ export const BeamConnectorTable = [{connector: "3 Joints, 3mm", weight: 0.75, ca
                                     {connector: "4 Hooks, 3mm", weight: 0.85, capacityPerLevel: 2500},
                                     {connector: "4 Hooks, 4mm", weight: 1.05, capacityPerLevel: 3500}]
 
-export const BeamWeightPerFootTable = [];
+export const BeamWeightPerFootTable = [
+  {beamSize: "80*50*2mm", weightPerFoot: 1.27},
+  {beamSize: "100*50*2mm", weightPerFoot: 1.46},
+  {beamSize: "110*50*2mm", weightPerFoot: 1.56},
+  {beamSize: "120*50*2mm", weightPerFoot: 1.66},
+  {beamSize: "140*50*2mm", weightPerFoot: 1.76},
+  {beamSize: "150*50*2mm", weightPerFoot: 1.86},
+  {beamSize: "160*50*2mm", weightPerFoot: 1.95}];
 
-export const BeamWeightCapacityTable = [];
+export const BoxBeamWeightCapacityTable = [
+[4.92,5.91,6.56,7.5,8.2,8.85,9.84,10.8,11.81,12.8,13.78],
+["80x50x2mm",3360,2333,1890,1429,1210,1037,840,694,569,467,383],
+];
 
 export const BracingTable = [
 {bracing: "25x25x1mm", capacityPerLevel: 1500, weightPerFoot: 0.30},
