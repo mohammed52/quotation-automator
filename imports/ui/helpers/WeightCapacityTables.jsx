4e5f18a6c05d07387c6eefb6dd0f66009c2e0d1e
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2017-04-22 16:53:31 +0500

    wip: calculate rack design

--- imports/ui/helpers/WeightCapacityTables.jsx
+++ imports/ui/helpers/WeightCapacityTables.jsx
@@ -1,29 +1,35 @@
 
 export const UprightWeightTable = 
-[{upright: "70x50, 2mm", weightPerFoot: 0.75, capacityPerRack: 1500},
-  {upright: "80x70, 2mm", weightPerFoot: 1.06, capacityPerRack: 8000},
-  {upright: "80x70, 3mm", weightPerFoot: 1.58, capacityPerRack: 12000},
-  {upright: "100x80, 2mm ", weightPerFoot: 1.71, capacityPerRack: 14000},
-  {upright: "100x80, 2.5mm ", weightPerFoot: 2.13, capacityPerRack: 16000}]
+[{upright: "70x50x2mm", weightPerFoot: 0.75, capacityPerRack: 1500},
+  {upright: "80x70x2mm", weightPerFoot: 1.06, capacityPerRack: 8000},
+  {upright: "80x70x3mm", weightPerFoot: 1.58, capacityPerRack: 12000},
+  {upright: "100x80x2mm ", weightPerFoot: 1.71, capacityPerRack: 14000},
+  {upright: "100x80x2.5mm ", weightPerFoot: 2.13, capacityPerRack: 16000}]
 
 export const BeamConnectorTable = [{connector: "3 Joints, 3mm", weight: 0.75, capacityPerLevel: 1000},
                                     {connector: "4 Hooks, 3mm", weight: 0.85, capacityPerLevel: 2500},
                                     {connector: "4 Hooks, 4mm", weight: 1.05, capacityPerLevel: 3500}]
 
 export const BeamWeightPerFootTable = [
-  {beamSize: "80*50*2mm", weightPerFoot: 1.27},
-  {beamSize: "100*50*2mm", weightPerFoot: 1.46},
-  {beamSize: "110*50*2mm", weightPerFoot: 1.56},
-  {beamSize: "120*50*2mm", weightPerFoot: 1.66},
-  {beamSize: "140*50*2mm", weightPerFoot: 1.76},
-  {beamSize: "150*50*2mm", weightPerFoot: 1.86},
-  {beamSize: "160*50*2mm", weightPerFoot: 1.95}];
+  {boxBeamSize: "80x50x2mm", weightPerFoot: 1.27},
+  {boxBeamSize: "100x50x2mm", weightPerFoot: 1.46},
+  {boxBeamSize: "110x50x2mm", weightPerFoot: 1.56},
+  {boxBeamSize: "120x50x2mm", weightPerFoot: 1.66},
+  {boxBeamSize: "140x50x2mm", weightPerFoot: 1.76},
+  {boxBeamSize: "150x50x2mm", weightPerFoot: 1.86},
+  {boxBeamSize: "160x50x2mm", weightPerFoot: 1.95}];
 
 export const BoxBeamWeightCapacityTable = [
-[4.92,5.91,6.56,7.5,8.2,8.85,9.84,10.8,11.81,12.8,13.78],
-["80x50x2mm",3360,2333,1890,1429,1210,1037,840,694,569,467,383],
+  [4.92,5.91,6.56,7.5,8.2,8.85,9.84,10.8,11.81,12.8,13.78],
+  ["80x50x2mm",3360,2333,1890,1429,1210,1037,840,694,569,467,383],
+  ["100x50x2mm",4000,3928,3128,2406,2036,1746,1414,1169,959,786,645],
+  ["110x50x2mm",4000,4000,3870,2926,2477,2123,1720,1421,1165,955,783],
+  ["120x50x2mm",4000,4000,4000,3692,3125,2679,2170,1793,1470,1206,989],
+  ["140x50x2mm",4000,4000,4000,4000,4000,3854,3122,2580,2116,1735,1423],
+  ["150x50x2mm",4000,4000,4000,4000,4000,4000,3952,3266,2678,2196,1801],
+  ["160x50x2mm",4000,4000,4000,4000,4000,6593,5340,4413,3619,2967,2433]
 ];
 
 export const BracingTable = [
-{bracing: "25x25x1mm", capacityPerLevel: 1500, weightPerFoot: 0.30},
-{bracing: "25x25x1.2mm", capacityPerLevel: 1500, weightPerFoot: 0.35}];
+{bracing: "25x25x1.2mm", capacityPerLevel: 1500, weightPerFoot: 0.30},
+{bracing: "25x25x1.5mm", capacityPerLevel: 1500, weightPerFoot: 0.35}];
