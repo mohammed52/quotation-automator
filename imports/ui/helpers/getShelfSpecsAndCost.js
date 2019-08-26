f294188b30a05c636eb7fc57621a8814241d801c
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2019-08-26 10:46:06 +0500

    wip: updated shelf weight with 1.5 inches excess size on each side

--- imports/ui/helpers/getShelfSpecsAndCost.js
+++ imports/ui/helpers/getShelfSpecsAndCost.js
@@ -1,12 +1,11 @@
 // import { getMaxRackCapacityReqd } from './getMaxRackCapacityReqd'
 // import { getMaxLevelCapacity } from './getMaxLevelCapacity'
 // import { UprightWeightTable } from './WeightCapacityTables'
-// import {generateLog } from './generateLog' 
-import { getQtyOfShelves } from './getQtyOfShelves'
+// import {generateLog } from './generateLog'
+import { getQtyOfShelves } from "./getQtyOfShelves";
 
 export function getShelfSpecsAndCost(rackingRequirements) {
-
-  const MAPLOG = false
+  const MAPLOG = false;
 
   var shelfSpecsAndCost = {
     description: "",
@@ -15,35 +14,32 @@ export function getShelfSpecsAndCost(rackingRequirements) {
   };
 
   let weightPerSqInch = 0;
-  let tmpDescription = ""
+  let tmpDescription = "";
   const size = rackingRequirements.frame.frameDepth + "feet x 1feet x ";
-  const shelfTotalQty = getQtyOfShelves(rackingRequirements)
+  const shelfTotalQty = getQtyOfShelves(rackingRequirements);
 
   if (rackingRequirements.shelfType == "metalShelf1.0mm") {
-    weightPerSqInch = 0.005512407
-    tmpDescription = "Metal Shelf " + size + "1.0mm"
-
+    weightPerSqInch = 0.005512407;
+    tmpDescription = "Metal Shelf " + size + "1.0mm";
   } else if (rackingRequirements.shelfType == "metalShelf1.2mm") {
-    weightPerSqInch = 0.006299894
-    tmpDescription = "Metal Shelf " + size + "1.2mm"
-
+    weightPerSqInch = 0.006299894;
+    tmpDescription = "Metal Shelf " + size + "1.2mm";
   } else if (rackingRequirements.shelfType == "metalShelf1.5mm") {
-    weightPerSqInch = 0.007874867
-    tmpDescription = "Metal Shelf " + size + "1.5mm"
+    weightPerSqInch = 0.007874867;
+    tmpDescription = "Metal Shelf " + size + "1.5mm";
   } else if (rackingRequirements.shelfType == "metalShelf2.0mm") {
-    weightPerSqInch = 0.009843584
-    tmpDescription = "Metal Shelf " + size + "2.0mm"
+    weightPerSqInch = 0.009843584;
+    tmpDescription = "Metal Shelf " + size + "2.0mm";
   }
 
-  const frameDepth = rackingRequirements.frame.frameDepth
+  const frameDepth = rackingRequirements.frame.frameDepth;
 
+  let shelfUnitWeight = 15 * (frameDepth * 12 + 3) * weightPerSqInch;
 
-  let shelfUnitWeight = 16 * (frameDepth * 12 + 4) * weightPerSqInch
-
-  shelfSpecsAndCost.description = tmpDescription
-  shelfSpecsAndCost.unitWeight = shelfUnitWeight
-  shelfSpecsAndCost.qty = shelfTotalQty
+  shelfSpecsAndCost.description = tmpDescription;
+  shelfSpecsAndCost.unitWeight = shelfUnitWeight;
+  shelfSpecsAndCost.qty = shelfTotalQty;
 
   if (MAPLOG) console.log("shelfSpecsAndCost", shelfSpecsAndCost);
-  return shelfSpecsAndCost
+  return shelfSpecsAndCost;
 }
